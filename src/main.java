937cc0f41daf9b4736170b79cb6fa667aec4306d
12116809 <lestianm@gmail.com>
2023-05-23 22:11:12 -0300

    feat: cria connection

---
+++ src/main.java
@@ -0,0 +1,46 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class main {
+    public static void main(String[] args) {
+
+        Connection connection = null;
+
+        try {
+            //Load the JDBC driver
+            String driverName = "org.gjt.mm.mysql.Driver";
+            Class.forName(driverName);
+
+            //Create a connection to the database
+            String serverName = "localhost";
+            String mydatabase = "bd_rh";
+            String url = "jdbc:mysql://" + serverName + "/" + mydatabase;
+            String username = "username";
+            String password = "password";
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException e) {
+            //Could not find the database driver
+            String msg = "erro1";
+        } catch (SQLException e) {
+            //Could not connect to the database
+            String msg = "erro2";
+        }
+
+        try {
+            Statement stmt = connection.createStatement();
+
+            //Use TRUNCATE
+            String sql = "TRUNCATE my_table";
+
+            //Use DELETE
+            sql = "DELETE FROM my_table";
+
+            //Execute deletion
+            stmt.executeUpdate(sql);
+        } catch (SQLException e) {
+            String msg = "erro3";
+        }
+    }
+}
